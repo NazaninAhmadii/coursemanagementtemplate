0cfdab33234d1735c5fa7bac6022bafd684db1e1
NazaninAhmadii <nazaninpolite@gmail.com>
2022-03-24 14:09:55 -0700

    added redis client

--- server.js
+++ server.js
@@ -3,6 +3,20 @@ const { engine } = require("express-handlebars");
 const path = require("path");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
+const Redis = require("ioredis");
+
+// Create redis client
+const redisClient = new Redis({
+    host: '127.0.0.1',
+    port: 9000
+});
+
+redisClient.on('connect', () =>{
+    console.log('Redis connection successful ... ');
+});
+redisClient.on('error', ()=>{
+    console.log('Redis ERROR: no connection!!! ');
+})
 
 // Set port
 var PORT = process.env.PORT || 3000;
@@ -27,10 +41,58 @@ app.get('/', function(req, res, next) {
     res.render('searchcourses');
 });
 
+// process searching
+app.post('/course/search', async function(req, res) {
+    const id = req.body.id;
+    const obj = await redisClient.hgetall(id);
+    console.log(obj);
+    if(!obj.name) {
+        res.render('searchcourses', {
+            error: 'Course does not exist!'
+        })
+    } else {
+        obj.id = id;
+        res.render('details', {
+            course: obj
+        });
+    }
+});
+
 // Add Course Page
 app.get('/course/add', function(req, res, next) {
     res.render('addcourse');
 });
+
+// process add course
+app.post('/course/add', async function(req, res) {
+    let id = req.body.id;
+    let name = req.body.name;
+    let instructor = req.body.instructor;
+    let startdate = req.body.startdate;
+    let period = req.body.period;
+
+    try {
+        await redisClient.hmset(id, [
+            'name', name,
+            'instructor', instructor,
+            'startdate', startdate,
+            'period', period
+        ]);
+        res.redirect('/');
+    } catch(error) {
+        console.log(`Something went wrong : ${error}`);
+    }
+});
+
+// delete course
+app.delete('/course/delete/:id', async function(req, res) {
+    try{
+        await redisClient.del(req.params.id);
+        res.redirect('/');
+    } catch (error) {
+        console.log(`Something went wrong while deleting data: ${error}`);
+    }
+})
 
 app.listen(PORT, function(error) {
     if(error) throw error;
